ba32c96760e255abbcd0ff44b182ea746457c881
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-11-26 23:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function createTable() {
+	
     let rn = Number(prompt("Input number of rows"));
 	let cn = Number(prompt("Input number of columns"));
 
 	let table = document.createElement('table');
+	table.id = "myTable"
 	for(let i=0;i<rn;i++){
 		let tr = document.createElement('tr');
+		tr.id = "myTable tr"
 		for(let j=0;j<cn;j++){
 			let td = document.createElement('td');
+			td.id = "myTable td"
 			td.innerText = `Row-${i} Column-${j}`;
 			tr.append(td);
 		}		
